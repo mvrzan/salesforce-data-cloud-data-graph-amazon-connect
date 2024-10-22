e400825e5a908ceabac1e07bdbce1b95631b7185
mvrzan <mvrzan@icloud.com>
2024-10-22 09:50:11 -0400

    update data graph parsing logic to be more dynamic

--- aws/src/utils/parseDataGraph.mjs
+++ aws/src/utils/parseDataGraph.mjs
@@ -9,141 +9,87 @@ export const parseDataGraph = (dataGraph) => {
 
   const decodedJson = decodeJson(dataGraph.data[0].json_blob__c);
   const parsedJson = JSON.parse(decodedJson);
+  const individual = parsedJson.UnifiedssotIndividualPat__dlm[0];
+  const linkIndividuals = individual.UnifiedLinkssotIndividualPat__dlm;
+
+  // website engagement data
+  const individualWebsiteEngagementData = linkIndividuals.find((link) =>
+    link.ssot__Individual__dlm.some((item) => item.hasOwnProperty("ssot__WebsiteEngagement__dlm"))
+  );
+
+  const websiteEngagement = {
+    engagementDate:
+      individualWebsiteEngagementData.ssot__Individual__dlm[0].ssot__WebsiteEngagement__dlm[0]
+        .ssot__EngagementDateTm__c,
+    webPageCategory:
+      individualWebsiteEngagementData.ssot__Individual__dlm[0].ssot__WebsiteEngagement__dlm[0].Webpage_Category__c,
+  };
+
+  // email engagement data
+  const individualEmailEngagementData = linkIndividuals.find((link) =>
+    link.ssot__Individual__dlm.some((item) => item.hasOwnProperty("ssot__EmailEngagement__dlm"))
+  );
+
+  const emailEngagement = {
+    engagementDate:
+      individualEmailEngagementData.ssot__Individual__dlm[0].ssot__EmailEngagement__dlm[0].ssot__EngagementDateTm__c,
+    emailSubject:
+      individualEmailEngagementData.ssot__Individual__dlm[0].ssot__EmailEngagement__dlm[0].ssot__SubjectLineTxt__c,
+    emailName: individualEmailEngagementData.ssot__Individual__dlm[0].ssot__EmailEngagement__dlm[0].ssot__EmailName__c,
+    engagementType:
+      individualEmailEngagementData.ssot__Individual__dlm[0].ssot__EmailEngagement__dlm[0].ssot__EngagementTypeId__c,
+    emailSentDate:
+      individualEmailEngagementData.ssot__Individual__dlm[0].ssot__EmailEngagement__dlm[0].ssot__SentDateTm__c,
+  };
+
+  // SMS engagement data
+  const individualSmsEngagementData = linkIndividuals.find((link) =>
+    link.ssot__Individual__dlm.some((item) => item.hasOwnProperty("ssot__MessageEngagement__dlm"))
+  );
+
+  const smsEngagement = {
+    engagementDate:
+      individualSmsEngagementData.ssot__Individual__dlm[0].ssot__MessageEngagement__dlm[0].ssot__EngagementDateTm__c,
+    engagementType:
+      individualSmsEngagementData.ssot__Individual__dlm[0].ssot__MessageEngagement__dlm[0].ssot__EngagementTypeId__c,
+    keywordText:
+      individualSmsEngagementData.ssot__Individual__dlm[0].ssot__MessageEngagement__dlm[0].ssot__KeywordTxt__c,
+    smsName: individualSmsEngagementData.ssot__Individual__dlm[0].ssot__MessageEngagement__dlm[0].ssot__Name__c,
+    sendTimePhoneNumber:
+      individualSmsEngagementData.ssot__Individual__dlm[0].ssot__MessageEngagement__dlm[0].ssot__SendTimePhoneNumber__c,
+    sentDate: individualSmsEngagementData.ssot__Individual__dlm[0].ssot__MessageEngagement__dlm[0].ssot__SentDateTm__c,
+    templateName: individualSmsEngagementData.ssot__Individual__dlm[0].ssot__MessageEngagement__dlm[0].Tempate_Name,
+  };
+
+  // latest appointment data
+  const individualAppointmentData = linkIndividuals.find((link) =>
+    link.ssot__Individual__dlm.some((item) => item.hasOwnProperty("Appointment__dlm"))
+  );
+
+  const latestAppointment = {
+    appointmentTime: individualAppointmentData.ssot__Individual__dlm[0].Appointment__dlm[0].Appointment_Time_c__c,
+    createdDate: individualAppointmentData.ssot__Individual__dlm[0].Appointment__dlm[0].CreatedDate__c,
+    name: individualAppointmentData.ssot__Individual__dlm[0].Appointment__dlm[0].Name__c,
+    procedure: individualAppointmentData.ssot__Individual__dlm[0].Appointment__dlm[0].Procedure_c__c,
+    procedureId: individualAppointmentData.ssot__Individual__dlm[0].Appointment__dlm[0].Procedure_Id__c,
+    status: individualAppointmentData.ssot__Individual__dlm[0].Appointment__dlm[0].Status_c__c,
+  };
 
   const formattedDataGraphObject = {
-    firstName: parsedJson.UnifiedssotIndividualPat__dlm[0].ssot__FirstName__c,
-    lastName: parsedJson.UnifiedssotIndividualPat__dlm[0].ssot__LastName__c,
-    yearlyIncome:
-      parsedJson.UnifiedssotIndividualPat__dlm[0].ssot__YearlyIncomeRangeId__c,
+    firstName: individual.ssot__FirstName__c,
+    lastName: individual.ssot__LastName__c,
+    yearlyIncome: individual.ssot__YearlyIncomeRangeId__c,
     phoneNumber: parsedJson.ssot__FormattedE164PhoneNumber__c,
-    emailAddress:
-      parsedJson.UnifiedssotIndividualPat__dlm[0]
-        .UnifiedssotContactPointEmailPat__dlm[0].ssot__EmailAddress__c,
-    address:
-      parsedJson.UnifiedssotIndividualPat__dlm[0]
-        .UnifiedssotContactPointAddressPat__dlm[0].ssot__AddressLine1__c,
-    city: parsedJson.UnifiedssotIndividualPat__dlm[0]
-      .UnifiedssotContactPointAddressPat__dlm[0].ssot__CityId__c,
-    country:
-      parsedJson.UnifiedssotIndividualPat__dlm[0]
-        .UnifiedssotContactPointAddressPat__dlm[0].ssot__CountryId__c,
-    postalCode:
-      parsedJson.UnifiedssotIndividualPat__dlm[0]
-        .UnifiedssotContactPointAddressPat__dlm[0].ssot__PostalCodeId__c,
-    state:
-      parsedJson.UnifiedssotIndividualPat__dlm[0]
-        .UnifiedssotContactPointAddressPat__dlm[0].ssot__StateProvinceId__c,
-    websiteEngagement: {
-      engagementDate:
-        parsedJson.UnifiedssotIndividualPat__dlm[0]
-          .UnifiedLinkssotIndividualPat__dlm[0].ssot__Individual__dlm[0]
-          .ssot__WebsiteEngagement__dlm[0].ssot__EngagementDateTm__c,
-      webPageCategory:
-        parsedJson.UnifiedssotIndividualPat__dlm[0]
-          .UnifiedLinkssotIndividualPat__dlm[0].ssot__Individual__dlm[0]
-          .ssot__WebsiteEngagement__dlm[0].Webpage_Category__c,
-    },
-    emailEngagement: {
-      engagementDate:
-        parsedJson.UnifiedssotIndividualPat__dlm[0]
-          .UnifiedLinkssotIndividualPat__dlm[1].ssot__Individual__dlm[0]
-          .ssot__EmailEngagement__dlm[0].ssot__EngagementDateTm__c,
-      emailSubject:
-        parsedJson.UnifiedssotIndividualPat__dlm[0]
-          .UnifiedLinkssotIndividualPat__dlm[1].ssot__Individual__dlm[0]
-          .ssot__EmailEngagement__dlm[0].ssot__SubjectLineTxt__c,
-      emailName:
-        parsedJson.UnifiedssotIndividualPat__dlm[0]
-          .UnifiedLinkssotIndividualPat__dlm[1].ssot__Individual__dlm[0]
-          .ssot__EmailEngagement__dlm[0].ssot__EmailName__c,
-      engagementType:
-        parsedJson.UnifiedssotIndividualPat__dlm[0]
-          .UnifiedLinkssotIndividualPat__dlm[1].ssot__Individual__dlm[0]
-          .ssot__EmailEngagement__dlm[0].ssot__EngagementTypeId__c,
-      emailSentDate:
-        parsedJson.UnifiedssotIndividualPat__dlm[0]
-          .UnifiedLinkssotIndividualPat__dlm[1].ssot__Individual__dlm[0]
-          .ssot__EmailEngagement__dlm[0].ssot__SentDateTm__c,
-    },
-    smsEngagement: {
-      engagementDate:
-        parsedJson.UnifiedssotIndividualPat__dlm[0]
-          .UnifiedLinkssotIndividualPat__dlm[1].ssot__Individual__dlm[0]
-          .ssot__MessageEngagement__dlm[0].ssot__EngagementDateTm__c,
-      engagementType:
-        parsedJson.UnifiedssotIndividualPat__dlm[0]
-          .UnifiedLinkssotIndividualPat__dlm[1].ssot__Individual__dlm[0]
-          .ssot__MessageEngagement__dlm[0].ssot__EngagementTypeId__c,
-      keywordText:
-        parsedJson.UnifiedssotIndividualPat__dlm[0]
-          .UnifiedLinkssotIndividualPat__dlm[1].ssot__Individual__dlm[0]
-          .ssot__MessageEngagement__dlm[0].ssot__KeywordText__c,
-      smsName:
-        parsedJson.UnifiedssotIndividualPat__dlm[0]
-          .UnifiedLinkssotIndividualPat__dlm[1].ssot__Individual__dlm[0]
-          .ssot__MessageEngagement__dlm[0].ssot__Name__c,
-      sendTimePhoneNumber:
-        parsedJson.UnifiedssotIndividualPat__dlm[0]
-          .UnifiedLinkssotIndividualPat__dlm[1].ssot__Individual__dlm[0]
-          .ssot__MessageEngagement__dlm[0].ssot__SendTimePhoneNumber__c,
-      sentDate:
-        parsedJson.UnifiedssotIndividualPat__dlm[0]
-          .UnifiedLinkssotIndividualPat__dlm[1].ssot__Individual__dlm[0]
-          .ssot__MessageEngagement__dlm[0].ssot__SentDateTm__c,
-      templateName:
-        parsedJson.UnifiedssotIndividualPat__dlm[0]
-          .UnifiedLinkssotIndividualPat__dlm[1].ssot__Individual__dlm[0]
-          .ssot__MessageEngagement__dlm[0].Tempate_Name,
-    },
-    latestAppointment: {
-      appointmentTime:
-        parsedJson.UnifiedssotIndividualPat__dlm[0]
-          .UnifiedLinkssotIndividualPat__dlm[3].ssot__Individual__dlm[0]
-          .Appointment__dlm[
-          parsedJson.UnifiedssotIndividualPat__dlm[0]
-            .UnifiedLinkssotIndividualPat__dlm[3].ssot__Individual__dlm[0]
-            .Appointment__dlm.length - 1
-        ].Appointment_Time_c__c,
-      createdDate:
-        parsedJson.UnifiedssotIndividualPat__dlm[0]
-          .UnifiedLinkssotIndividualPat__dlm[3].ssot__Individual__dlm[0]
-          .Appointment__dlm[
-          parsedJson.UnifiedssotIndividualPat__dlm[0]
-            .UnifiedLinkssotIndividualPat__dlm[3].ssot__Individual__dlm[0]
-            .Appointment__dlm.length - 1
-        ].CreatedDate__c,
-      name: parsedJson.UnifiedssotIndividualPat__dlm[0]
-        .UnifiedLinkssotIndividualPat__dlm[3].ssot__Individual__dlm[0]
-        .Appointment__dlm[
-        parsedJson.UnifiedssotIndividualPat__dlm[0]
-          .UnifiedLinkssotIndividualPat__dlm[3].ssot__Individual__dlm[0]
-          .Appointment__dlm.length - 1
-      ].Name__c,
-      procedure:
-        parsedJson.UnifiedssotIndividualPat__dlm[0]
-          .UnifiedLinkssotIndividualPat__dlm[3].ssot__Individual__dlm[0]
-          .Appointment__dlm[
-          parsedJson.UnifiedssotIndividualPat__dlm[0]
-            .UnifiedLinkssotIndividualPat__dlm[3].ssot__Individual__dlm[0]
-            .Appointment__dlm.length - 1
-        ].Procedure_c__c,
-      procedureId:
-        parsedJson.UnifiedssotIndividualPat__dlm[0]
-          .UnifiedLinkssotIndividualPat__dlm[3].ssot__Individual__dlm[0]
-          .Appointment__dlm[
-          parsedJson.UnifiedssotIndividualPat__dlm[0]
-            .UnifiedLinkssotIndividualPat__dlm[3].ssot__Individual__dlm[0]
-            .Appointment__dlm.length - 1
-        ].Procedure_Id__c,
-      status:
-        parsedJson.UnifiedssotIndividualPat__dlm[0]
-          .UnifiedLinkssotIndividualPat__dlm[3].ssot__Individual__dlm[0]
-          .Appointment__dlm[
-          parsedJson.UnifiedssotIndividualPat__dlm[0]
-            .UnifiedLinkssotIndividualPat__dlm[3].ssot__Individual__dlm[0]
-            .Appointment__dlm.length - 1
-        ].Status_c__c,
-    },
+    emailAddress: individual.UnifiedssotContactPointEmailPat__dlm[0].ssot__EmailAddress__c,
+    address: individual.UnifiedssotContactPointAddressPat__dlm[0].ssot__AddressLine1__c,
+    city: individual.UnifiedssotContactPointAddressPat__dlm[0].ssot__CityId__c,
+    country: individual.UnifiedssotContactPointAddressPat__dlm[0].ssot__CountryId__c,
+    postalCode: individual.UnifiedssotContactPointAddressPat__dlm[0].ssot__PostalCodeId__c,
+    state: individual.UnifiedssotContactPointAddressPat__dlm[0].ssot__StateProvinceId__c,
+    websiteEngagement,
+    emailEngagement,
+    smsEngagement,
+    latestAppointment,
   };
 
   return formattedDataGraphObject;
